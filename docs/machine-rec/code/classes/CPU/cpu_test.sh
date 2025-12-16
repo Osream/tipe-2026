7b1b2284a51b6ff4ee1e856c8b8ff6c12c882a2b
Enzo <137120127+Osream@users.noreply.github.com>
2025-12-16 14:51:02 +0100

    text reading

--- docs/machine-rec/code/classes/CPU/cpu_test.sh
+++
@@ -1,5 +0,0 @@
-cd ../
-./comp.sh
-cd CPU
-g++ -Werror -Wextra -Wall -fsanitize=address -o output ../../object-files/cpu.o cpu_test.cpp
-./output
--- docs/machine-rec/code/comp.sh
+++
@@ -1,4 +0,0 @@
-# CPU class compiler
-cd classes/CPU
-g++ -Werror -Wall -Wextra -fsanitize=address -c cpu.cpp -o ../../object-files/cpu.o
-cd ../..
